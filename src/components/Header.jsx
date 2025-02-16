04968cc0dd5bd4105a8f99c69a03076b8fb87db6
parth <dasparth1544@gmail.com>
2025-02-16 22:48:18 +0530

    finally components

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header()
+{
+    return(
+        <>
+      
+        <h2>Parth Das</h2>
+        <div className="nav">
+        
+            <a href="">home</a> 
+            <a href="">blog</a> 
+            <a href="">music reviews?</a>
+            <a href="">essays?</a>
+            </div>
+            
+
+        
+        </>
+    )
+}
+export default Header;

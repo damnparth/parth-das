fe51889d276f4bb63c1ac645045e0bb55f4f5d7c
parth <dasparth1544@gmail.com>
2025-02-16 22:35:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+function App() {
+  
+  return (
+    <>
+    <Header />
+    <div>
+      <h3>this is a blog app. i figured most people have one so i wanna make one for myself. it looks cool to have one. </h3>
+      <h4>i will make it prettier later. i dont have any design in my mind for now.</h4>
+
+
+    </div>
+    <Footer />
+      
+    </>
+  )
+}
+
+export default App
